c0deca94e7b1fe4c20e974418bf290b3d9d8e6a8
jiechud <qiaojienet@163.com>
2018-12-27 16:13:38 +0800

    modify package.json

--- app/.umirc.js
+++ app/.umirc.js
@@ -1,6 +1,11 @@
 const path = require('path');
 
-const server = 'http://localhost:3000';
+let server = 'http://localhost:3000';
+if (process.env.SERVER === "TRUE") {
+  server = "http://39.105.188.65:8080";
+}
+
+console.log('server', server);
 
 // ref: https://umijs.org/config/
 export default {
